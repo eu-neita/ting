14d1a69cb294d9a219a7a2e4ded4e3b3ea8331c4
nathan crevelin <neitass.music@gmail.com>
2023-10-02 19:41:43 -0300

    feat: exists_word dunction but is not validated, width not inexistent word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for index in range(len(instance)):
+        file_info = instance.search(index)
+
+        lines_with_word = []
+        num = enumerate(file_info["linhas_do_arquivo"], start=1)
+        for line_index, line in num:
+            if word.lower() in line.lower():
+                lines_with_word.append({"linha": line_index})
+
+        if lines_with_word:
+            result_entry = {
+                "palavra": word,
+                "arquivo": file_info["nome_do_arquivo"],
+                "ocorrencias": lines_with_word
+            }
+            results.append(result_entry)
+
+    return results
 
 
 def search_by_word(word, instance):
