42e959d48a9c6f634c12b7b5fd768f332bfafb8f
nathan crevelin <neitass.music@gmail.com>
2023-10-02 15:36:47 -0300

    feat: add txt_importer function verify if exists and .txt format, fix: lint

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,7 +1,14 @@
 import os
 import sys
 
+
 def txt_importer(path_file):
+    if not os.path.exists(path_file):
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return
+    if not path_file.endswith('.txt'):
+        sys.stderr.write("Formato inválido\n")
+        return
     with open(path_file, 'r') as file:
         lines = file.read().split("\n")
     return lines
